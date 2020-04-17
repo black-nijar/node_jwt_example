0cc4ea34f27b9f2218f507e6d5302f9a86df5a27
Nijarudeen <nijarr2020@gmail.com>
2020-04-17 14:07:24 +0530

    token created

--- app.js
+++ app.js
@@ -9,4 +9,21 @@ app.get("/api", (req, res) => {
   });
 });
 
-app.listen(5000, () => console.log('Server started on port 5000'))
+app.post("/api/posts", (req, res) => {
+  res.json({
+    msg: "Post created",
+  });
+});
+
+app.post('/api/login', (req, res) => {
+  const user = {
+    id: 1,
+    name: 'Nijar',
+    email: 'nijar@g.com'
+  }
+  jwt.sign({ user }, 'secretkey', (err, token) => {
+    if(err) throw err;
+    res.json({ token });
+  })
+})
+app.listen(5000, () => console.log("Server started on port 5000"));
