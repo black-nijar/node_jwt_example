331df8395b5d84801891ba5732c2723c0b7d9362
Nijarudeen <nijarr2020@gmail.com>
2020-04-17 18:58:20 +0530

    token verified

--- app.js
+++ app.js
@@ -9,21 +9,47 @@ app.get("/api", (req, res) => {
   });
 });
 
-app.post("/api/posts", (req, res) => {
-  res.json({
-    msg: "Post created",
+app.post("/api/posts", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secret", (err, authData) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json({
+        msg: "Post created",
+        authData,
+      });
+    }
   });
 });
 
-app.post('/api/login', (req, res) => {
+app.post("/api/login", (req, res) => {
   const user = {
     id: 1,
-    name: 'Nijar',
-    email: 'nijar@g.com'
-  }
-  jwt.sign({ user }, 'secretkey', (err, token) => {
-    if(err) throw err;
+    name: "Nijar",
+    email: "nijar@g.com",
+  };
+  jwt.sign({ user }, "secret", (err, token) => {
+    if (err) throw err;
     res.json({ token });
-  })
-})
+  });
+});
+
+// Verify token
+function verifyToken(req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.headers.authorization;
+  if (typeof bearerHeader != "undefined") {
+    // Split the Space
+    const bearer = bearerHeader.split(" ");
+    // Get token from array
+    const bearerToken = bearer[1];
+    // Set the token
+    req.token = bearerToken;
+    // next middleware
+    next();
+  } else {
+    // Denied
+    res.sendStatus(403);
+  }
+}
 app.listen(5000, () => console.log("Server started on port 5000"));
